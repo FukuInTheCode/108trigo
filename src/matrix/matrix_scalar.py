dfd894b2ebb04bcf401f87274b827c089851a4dc
Tom C <tom.carlier@epitech.eu>
2024-03-20 14:40:39 +0100

    [del] -->  remove add the useless matrix functions

--- src/matrix/matrix_scalar.py
+++
@@ -1,5 +0,0 @@
-def matrix_scalar(matrix: list[list[float]], x: float) -> list[list[float]]:
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix[i][j] *= x
-    return matrix
--- src/matrix/matrix_transpose.py
+++
@@ -1,9 +0,0 @@
-def matrix_transpose(matrix):
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-    transposed_matrix = [[0] * num_rows for _ in range(num_cols)]
-    for i in range(num_rows):
-        for j in range(num_cols):
-            transposed_matrix[j][i] = matrix[i][j]
-
-    return transposed_matrix
