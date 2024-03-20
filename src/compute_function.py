83164bfe2e65908037fd967729e40d417c89a086
Tom C <tom.carlier@epitech.eu>
2024-03-20 15:47:48 +0100

    [refactor] -->  update file(s)

--- src/compute_function.py
+++ src/compute_function.py
@@ -1,13 +1,12 @@
 from src.matrix.matrix_sum import matrix_sum
 from src.matrix.matrix_pow import matrix_pow
 from math import factorial
+from src.print_result import print_result
 
 
 def compute_matrix_exp(A: list[list[float]]) -> int:
     result = [[0.0] * len(A[0]) for _ in range(len(A))]
 
-    for i in range(len(A)):
-        A[i][i] = 1
     for i in range(100):
         tmp = matrix_pow(A, i)
         coef = factorial(i)
