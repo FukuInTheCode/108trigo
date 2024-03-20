9ad266306692fb483d3bd283245b58bf7fac7d17
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-20 13:39:31 +0100

    [feat] -->  add the fucntion for display the usage of the project

---
+++ src/display_help.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def display_help() -> int:
+    print("USAGE")
+    print("\t./108trigo fun a0 a1 a2 ...\n")
+    print("DESCRIPTION")
+    print("\tfun\t function to be applied, among at least \"EXP\", \"COS\", \"SIN\", \"COSH\" and \"SINH\"")
+    print("\tai\t coeficients of the matrix")
+    return 0
